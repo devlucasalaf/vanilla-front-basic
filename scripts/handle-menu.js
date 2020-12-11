3da9414789b922e2f43cad1d61c108d4d84b8de7
devlucasalaf <68478705+devlucasalaf@users.noreply.github.com>
2020-12-11 14:59:38 -0300

    feat: open and close menu and scroll smooth

---
+++ scripts/handle-menu.js
@@ -0,0 +1,27 @@
+(function handleMenu(win, doc) {
+  let setOpen = false
+
+  const menuBtn = doc.querySelector('#menu-btn')
+  const menuContainer = doc.querySelector('#menu-container')
+  const links = doc.querySelectorAll('.links')
+  console.log(links)
+
+  const menuHandle = () => {
+    setOpen = !setOpen
+
+    if (setOpen === true) {
+      menuContainer.style.display = 'block'
+      menuBtn.innerHTML = 'Close'
+    } else {
+      menuContainer.style.display = 'none'
+      menuBtn.innerHTML = 'Open'
+    }
+  }
+
+  for (const link of links) {
+    link.addEventListener('click', menuHandle)
+  }
+  menuBtn.addEventListener('click', menuHandle, false)
+
+
+})(window, document)
---
+++ scripts/handle-scroll.js
@@ -0,0 +1,17 @@
+const links = document.querySelectorAll('.links');
+
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
+  console.log(offsetTop)
+
+  scroll({
+    top: offsetTop,
+    behavior: "smooth"
+  });
+}
